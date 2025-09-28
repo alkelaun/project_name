7d136a0049483a83356d2248ee22ba1693696b8f
John <8870050+alkelaun@users.noreply.github.com>
2025-09-28 13:13:13 -0700

    Update models.py

--- main/models.py
+++ main/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.db import models
+from django.conf import settings
+from uuid import uuid4
 
-# Create your models here.
+def protected_file_upload_path(instance, filename):
+    """
+    Creates a unique path for protected files to prevent direct access.
+    """
+    return f'protected/files/{uuid4()}_{filename}'
+
+def protected_image_upload_path(instance, filename):
+    """
+    Creates a unique path for protected images.
+    """
+    return f'protected/images/{uuid4()}_{filename}'
+
+class ProtectedFileBase(models.Model):
+    """
+    An abstract base model for all protected files.
+    """
+    file = models.FileField(upload_to=protected_file_upload_path)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+        
+class ProtectedImageBase(models.Model):
+    """
+    An abstract base model for all protected images.
+    """
+    image = models.ImageField(upload_to=protected_image_upload_path)
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+    
+    class Meta:
+        abstract = True
+
